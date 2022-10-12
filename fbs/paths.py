bb33186f69cb3c7486b90401a5e4546882b34044
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-12 16:25:53 +0100

    Added a module to allow configuration of fbs paths

---
+++ fbs/paths.py
@@ -0,0 +1,33 @@
+import os
+import json
+from functools import lru_cache
+from fbs import path
+from fbs.error import FbsError
+
+
+@lru_cache
+def _get_paths() -> dict:
+    paths_file = path("paths.json")
+    if os.path.isfile(paths_file):
+        try:
+            with open(paths_file) as f:
+                _paths = json.load(f)
+            if not isinstance(_paths, dict):
+                raise TypeError("paths.json file must contain a dictionary if defined.")
+        except Exception as e:
+            raise FbsError(e) from e
+    return _paths
+
+
+BuildPipelineDefault = "build_pipeline"
+IconsDefault = "icons"
+
+
+@lru_cache
+def get_build_path():
+    return path(_get_paths().get("build_path", BuildPipelineDefault))
+
+
+@lru_cache
+def get_icon_path():
+    return path(_get_paths().get("icons_path", "icons"))
