c89dec8764092520b71ac36cb945a98051b58291
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-18 10:39:13 +0100

    Renamed to pbt
    Renamed distance ourselves from the original.
    Keeping the old name implied some connection to the original.

--- fbs/sign_installer/windows.py
+++
@@ -1,8 +0,0 @@
-from fbs import SETTINGS
-from fbs.paths import project_path
-from fbs.sign.windows import sign_file
-
-
-def sign_installer_windows():
-    installer = project_path("target/${installer}")
-    sign_file(installer, SETTINGS["app_name"] + " Setup", SETTINGS["url"])
