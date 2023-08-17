38887e75e5dec73e5e1c334a1e7288fe8e461f61
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-17 15:31:54 +0100

    Remove extra dlls
    These seem to only be required on old systems which Python no longer supports

--- pbt/freeze/windows.py
+++ pbt/freeze/windows.py
@@ -30,45 +30,45 @@ def freeze_windows(debug=False):
         project_path("${build_system_dir}/icons/Icon.ico"),
         project_path("${freeze_dir}"),
     )
-    _add_missing_dlls()
+    # _add_missing_dlls()
 
 
-def _add_missing_dlls():
-    for dll_name in (
-        "msvcr100.dll",
-        "msvcr110.dll",
-        "msvcp110.dll",
-        "vcruntime140.dll",
-        "msvcp140.dll",
-        "concrt140.dll",
-        "vccorlib140.dll",
-    ):
-        try:
-            _add_missing_dll(dll_name)
-        except LookupError:
-            raise FileNotFoundError(
-                "Could not find %s on your PATH. Please install the Visual C++ "
-                "Redistributable for Visual Studio 2012 from:\n    "
-                "https://www.microsoft.com/en-us/download/details.aspx?id=30679"
-                % dll_name
-            ) from None
-    for ucrt_dll in ("api-ms-win-crt-multibyte-l1-1-0.dll",):
-        try:
-            _add_missing_dll(ucrt_dll)
-        except LookupError:
-            bitness_32_or_64 = struct.calcsize("P") * 8
-            raise FileNotFoundError(
-                "Could not find %s on your PATH. If you are on Windows 10, you "
-                "may have to install the Windows 10 SDK from "
-                "https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk. "
-                "Otherwise, try installing KB2999226 from "
-                "https://support.microsoft.com/en-us/kb/2999226. "
-                "In both cases, add the directory containing %s to your PATH "
-                "environment variable afterwards. If there are 32 and 64 bit "
-                "versions of the DLL, use the %s bit one (because that's the "
-                "bitness of your current Python interpreter)."
-                % (ucrt_dll, ucrt_dll, bitness_32_or_64)
-            ) from None
+# def _add_missing_dlls():
+#     for dll_name in (
+#         "msvcr100.dll",
+#         "msvcr110.dll",
+#         "msvcp110.dll",
+#         "vcruntime140.dll",
+#         "msvcp140.dll",
+#         "concrt140.dll",
+#         "vccorlib140.dll",
+#     ):
+#         try:
+#             _add_missing_dll(dll_name)
+#         except LookupError:
+#             raise FileNotFoundError(
+#                 "Could not find %s on your PATH. Please install the Visual C++ "
+#                 "Redistributable for Visual Studio 2012 from:\n    "
+#                 "https://www.microsoft.com/en-us/download/details.aspx?id=30679"
+#                 % dll_name
+#             ) from None
+#     for ucrt_dll in ("api-ms-win-crt-multibyte-l1-1-0.dll",):
+#         try:
+#             _add_missing_dll(ucrt_dll)
+#         except LookupError:
+#             bitness_32_or_64 = struct.calcsize("P") * 8
+#             raise FileNotFoundError(
+#                 "Could not find %s on your PATH. If you are on Windows 10, you "
+#                 "may have to install the Windows 10 SDK from "
+#                 "https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk. "
+#                 "Otherwise, try installing KB2999226 from "
+#                 "https://support.microsoft.com/en-us/kb/2999226. "
+#                 "In both cases, add the directory containing %s to your PATH "
+#                 "environment variable afterwards. If there are 32 and 64 bit "
+#                 "versions of the DLL, use the %s bit one (because that's the "
+#                 "bitness of your current Python interpreter)."
+#                 % (ucrt_dll, ucrt_dll, bitness_32_or_64)
+#             ) from None
 
 
 def _add_missing_dll(dll_name):
